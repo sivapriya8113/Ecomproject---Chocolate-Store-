1629d9404385f4eae0a6d864eb46ff32c603d81d
sivapriya8113 <68690248+sivapriya8113@users.noreply.github.com>
2022-06-03 12:44:47 +0530

    Update settings.py

--- Chocolate/Chocolate/settings.py
+++ Chocolate/Chocolate/settings.py
@@ -98,12 +98,13 @@ DATABASES = {
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        # 'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
         'knox.auth.TokenAuthentication',
     ]
 }
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
